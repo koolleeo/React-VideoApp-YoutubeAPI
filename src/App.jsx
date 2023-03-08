8c8ab49ace954d6ffde95003fa9d88134cdec450
Setup <setup@Leighs-iMac.local>
2023-03-08 12:21:48 +0000

    added browserRouter and routes. Also added Mui Box container

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>React VideoApp</h1>
-    </div>
+    <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
   )
+
 }
 
 export default App
