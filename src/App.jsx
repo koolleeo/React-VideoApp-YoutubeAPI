0cc07cd1e2650ea6e5b3902f1b7799fa59ff7e8a
Setup <setup@Leighs-iMac.local>
2023-03-08 09:59:23 +0000

    set up new react project using vite, set up json with package to install and added base css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>React VideoApp</h1>
+    </div>
+  )
+}
+
+export default App
