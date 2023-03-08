b02d1714d7103dd90b7233840ec5fd2b0580c843
Setup <setup@Leighs-iMac.local>
2023-03-08 13:00:49 +0000

    set up components folders and added rafce components - need to flesh out components with functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components'
+
 function App() {
 
   return (
     <BrowserRouter>
+    {/* Box (MUI) acts as a div container */}
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
+      {/* set up routes */}
       <Routes>
         <Route exact path='/' element={<Feed />} />
         <Route path='/video/:id' element={<VideoDetail />} />
