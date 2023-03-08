9a62631c538f15d0d73f7c58b2c1523e8fd1cb16
Leigh Ally <leigh.ally@gmail.com>
2023-03-08 17:26:03 +0000

    added section to feed component to render title and video tiles

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -12,7 +12,7 @@ const Feed = () => {
   return (
 
     <Stack sx={{ 
-              // side scroll at md (900px) breakpoint
+              // responsive design - stack on small screens (less than 900px)
               flexDirection: { sx: "column", md: "row" } 
               }}>
 
@@ -36,6 +36,26 @@ const Feed = () => {
         >
           Copyright © Koolleeo
         </Typography>
+
+      </Box>
+
+{/* section to render video title and tiles */}
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+
+        <Typography 
+                variant="h4" 
+                fontWeight="bold" 
+                mb={2} 
+                sx={{ color: "white" }}
+          >
+        {/* render video category title to the page */}
+
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+          
+        </Typography>
+
+        {/* add video logic here once videos component created */}
 
       </Box>
  
